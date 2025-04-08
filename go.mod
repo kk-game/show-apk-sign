86883b5597c34fe7653b39f037b8d9885fdc7f1c
kk-game <user88812345@gmail.com>
2025-04-08 11:09:04 +0800

    创建项目, 一个居于html的apk包签名查看工具

---
+++ go.mod
@@ -0,0 +1,7 @@
+module test
+
+go 1.23.0
+
+toolchain go1.23.8
+
+require golang.org/x/text v0.24.0 // indirect
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"html/template"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+type PageData struct {
+	Signature string
+}
+
+func main() {
+	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/upload", UploadFile)
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/favicon.ico")
+	})
+	http.ListenAndServe(":22222", nil)
+}
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, nil)
+}
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+	// 解析上传的文件
+	file, tempFile, err := parseAndSaveFile(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	defer tempFile.Close()
+
+	// 检查签名
+	signature, err := checkSignature(tempFile)
+	if err != nil {
+		http.Error(w, "Error Running Keytool Command", http.StatusInternalServerError)
+		return
+	}
+
+	// 返回 JSON 数据
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"signature": %q}`, signature)
+}
+
+func parseAndSaveFile(r *http.Request) (multipart.File, *os.File, error) {
+	r.ParseMultipartForm(500 << 20) // 限制上传大小为500MB
+	file, _, err := r.FormFile("apkfile")
+	if err != nil {
+		return nil, nil, fmt.Errorf("error retrieving the file")
+	}
+
+	tempFile, err := os.CreateTemp("", "upload-*.apk")
+	if err != nil {
+		return nil, nil, fmt.Errorf("error creating temp file")
+	}
+
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error writing file content")
+	}
+
+	return file, tempFile, nil
+}
+
+func checkSignature(tempFile *os.File) (string, error) {
+	cmd := exec.Command("keytool", "-printcert", "-jarfile", tempFile.Name())
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("error running keytool command")
+	}
+
+	// 将 GBK 编码转换为 UTF-8
+	reader := transform.NewReader(&out, simplifiedchinese.GBK.NewDecoder())
+	utf8Bytes, err := io.ReadAll(reader)
+	if err != nil {
+		return "", fmt.Errorf("error decoding GBK output: %v", err)
+	}
+
+	return string(utf8Bytes), nil
+}
+
+func displayResult(w http.ResponseWriter, signature string) {
+	data := PageData{
+		Signature: signature,
+	}
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, data)
+}
